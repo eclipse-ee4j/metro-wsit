d90548dd93b14640ef6eaf82126dcd0d3db422e4
Lukas Jungmann <lukas.jungmann@oracle.com>
2018-12-14 01:06:29 +0100

    avoid unneeded reflection call
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/wss/provider/AuthParamHelper.java
+++ wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/wss/provider/AuthParamHelper.java
@@ -10,7 +10,6 @@
 
 package com.sun.xml.wss.provider;
 
-import java.lang.reflect.Method;
 import javax.security.auth.message.MessageInfo;
 import javax.xml.soap.SOAPMessage;
 
@@ -22,34 +21,22 @@ class AuthParamHelper {
 
     @SuppressWarnings("unchecked")
     static SOAPMessage getRequest(MessageInfo param) {
-        try {
-            Class clz = param.getClass();
-            Method meth = clz.getMethod("getRequestMessage", (Class[]) null);
-            if (meth != null) {
-                Object obj = meth.invoke(param, (Object[]) null);
-                if (obj instanceof SOAPMessage) {
-                    return (SOAPMessage)obj;
-                }
+        if (param != null) {
+            Object obj = param.getRequestMessage();
+            if (obj instanceof SOAPMessage) {
+                return (SOAPMessage) obj;
             }
-
-        } catch (ReflectiveOperationException | SecurityException | IllegalArgumentException ex) {
         }
         return null;
     }
 
     @SuppressWarnings("unchecked")
     static SOAPMessage getResponse(MessageInfo param) {
-         try {
-            Class clz = param.getClass();
-            Method meth = clz.getMethod("getResponseMessage", (Class[]) null);
-            if (meth != null) {
-                Object obj = meth.invoke(param, (Object[]) null);
-                if (obj instanceof SOAPMessage) {
-                    return (SOAPMessage)obj;
-                }
+        if (param != null) {
+            Object obj = param.getResponseMessage();
+            if (obj instanceof SOAPMessage) {
+                return (SOAPMessage) obj;
             }
-
-        } catch (ReflectiveOperationException | SecurityException | IllegalArgumentException ex) {
         }
         return null;
     }
