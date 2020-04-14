8f7d21a3d34ed953efc71a982ddca8120d68b890
Lukas Jungmann <lukas.jungmann@oracle.com>
2020-04-15 00:07:26 +0200

    put back removed apis to stay backward compatibility in order to allow testing
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

---
+++ wsit/metro-runtime/metro-runtime-api/src/main/java/com/sun/xml/ws/assembler/ClientPipelineHook.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0, which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package com.sun.xml.ws.assembler;
+
+import com.sun.xml.ws.assembler.dev.*;
+
+/**
+ * @deprecated To be removed in 3.0, use com.sun.xml.ws.assembler.metro.dev.ClientPipelineHook instead.
+ */
+@Deprecated
+public class ClientPipelineHook extends com.sun.xml.ws.assembler.dev.ClientPipelineHook{
+
+}
---
+++ wsit/metro-runtime/metro-runtime-api/src/main/java/com/sun/xml/ws/assembler/ServerPipelineHook.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0, which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+package com.sun.xml.ws.assembler;
+
+/**
+ * @deprecated To be removed in 3.0, use com.sun.xml.ws.assembler.metro.ServerPipelineHook instead.
+ */
+@Deprecated
+public class ServerPipelineHook extends com.sun.xml.ws.assembler.metro.ServerPipelineHook {
+
+}
