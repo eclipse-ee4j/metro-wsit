efe611ff549260a8c2af1fc6f7c547fd734036d6
Lukas Jungmann <lukas.jungmann@oracle.com>
2021-12-13 22:11:36 +0100

    bump src/bin level to JDK 11, drop dependency on xmlresolver, remove dependency on JDK internal APIs
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- wsit/ws-sx/wssx-impl/src/test/java/com/sun/xml/wss/callback/PolicyCallbackHandler1.java
+++ wsit/ws-sx/wssx-impl/src/test/java/com/sun/xml/wss/callback/PolicyCallbackHandler1.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -563,47 +563,58 @@ public  class PolicyCallbackHandler1 implements CallbackHandler {
         }
     }
 
+    /**
+     * total header length
+     */
+    private static final byte HEADER_LENGTH = 4;
+
+    /**
+     * length of a header value
+     */
+    private static final byte HEADER_FIELD_LENGTH = 1;
+
+    /**
+     * position (0-based) of the header field that contains the expected length of the actual value in byte
+     */
+    private static final byte HEADER_FIELD_VALUE_LENGTH = 3;
+
     private static byte[] getSubjectKeyIdentifier(X509Certificate cert) {
         String SUBJECT_KEY_IDENTIFIER_OID = "2.5.29.14";
-        byte[] subjectKeyIdentifier =
-            cert.getExtensionValue(SUBJECT_KEY_IDENTIFIER_OID);
+        byte[] subjectKeyIdentifier = cert.getExtensionValue(SUBJECT_KEY_IDENTIFIER_OID);
         if (subjectKeyIdentifier == null)
             return null;
-         
+
         try {
-            sun.security.x509.KeyIdentifier keyId = null; 
-         
-            sun.security.util.DerValue derVal = new sun.security.util.DerValue(
-                 new sun.security.util.DerInputStream(subjectKeyIdentifier).getOctetString());
-         
-            keyId = new sun.security.x509.KeyIdentifier(derVal.getOctetString());
-            return keyId.getIdentifier();
-        } catch (NoClassDefFoundError ncde) {
-            if (subjectKeyIdentifier == null)
-                return null;
-            byte[] dest = new byte[subjectKeyIdentifier.length - 4];
-            System.arraycopy(
-                subjectKeyIdentifier, 4, dest, 0, subjectKeyIdentifier.length - 4);
-            return dest;
-        } catch ( java.io.IOException ex) {
-            //ignore
+            if (subjectKeyIdentifier.length < HEADER_LENGTH) {
+                throw new IllegalArgumentException("subjectKeyIdentifier too short, header is missing");
+            }
+
+            final int valueLength = (new BigInteger(
+                    Arrays.copyOfRange(subjectKeyIdentifier, HEADER_FIELD_VALUE_LENGTH, HEADER_FIELD_VALUE_LENGTH + HEADER_FIELD_LENGTH)))
+                    .intValue();
+            final int expectedTotalLength = HEADER_LENGTH + valueLength;
+            if (subjectKeyIdentifier.length < expectedTotalLength) {
+                throw new IllegalArgumentException(String.format("subjectKeyIdentifier too short (expected=%d, actual=%d)", expectedTotalLength, subjectKeyIdentifier.length));
+            } else if (subjectKeyIdentifier.length > expectedTotalLength) {
+                throw new IllegalArgumentException(String.format("subjectKeyIdentifier too big (expected=%d, actual=%d)", expectedTotalLength, subjectKeyIdentifier.length));
+            }
+            return Arrays.copyOfRange(subjectKeyIdentifier, HEADER_LENGTH, HEADER_LENGTH + valueLength);
+
+        } catch (Exception e) {
             return null;
         }
     }
 
 
-    private class PlainTextPasswordValidator implements PasswordValidationCallback.PasswordValidator {
+    private static class PlainTextPasswordValidator implements PasswordValidationCallback.PasswordValidator {
 
         public boolean validate(PasswordValidationCallback.Request request)
             throws PasswordValidationCallback.PasswordValidationException {
             
             PasswordValidationCallback.PlainTextPasswordRequest plainTextRequest =
                 (PasswordValidationCallback.PlainTextPasswordRequest) request;
-            if ("Ron".equals(plainTextRequest.getUsername()) &&
-                "noR".equals(plainTextRequest.getPassword())) {
-                return true;
-            }
-            return false;
+            return "Ron".equals(plainTextRequest.getUsername()) &&
+                    "noR".equals(plainTextRequest.getPassword());
         }
     }
 
