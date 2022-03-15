c695509912cab38e765b88ce77df20185d92052b
Lukas Jungmann <lukas.jungmann@oracle.com>
2022-03-15 18:09:47 +0100

    Integrate Santuario 2.3.2
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>
    (cherry picked from commit 301632085421239f6b68c7f735eda12212bbc6f9)

--- wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/wss/impl/transform/AttachmentContentOnlyTransform.java
+++ wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/wss/impl/transform/AttachmentContentOnlyTransform.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -21,40 +21,51 @@ import com.sun.xml.wss.impl.c14n.CanonicalizerFactory;
 
 import com.sun.xml.wss.impl.resolver.AttachmentSignatureInput;
 
+import org.apache.xml.security.c14n.CanonicalizationException;
+import org.apache.xml.security.c14n.InvalidCanonicalizerException;
 import org.apache.xml.security.transforms.TransformSpi;
-import org.apache.xml.security.signature.XMLSignatureInput; 
+import org.apache.xml.security.signature.XMLSignatureInput;
 import org.apache.xml.security.transforms.TransformationException;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class AttachmentContentOnlyTransform extends TransformSpi {
 
    private static final String implementedTransformURI =
-          "http://docs.oasis-open.org/wss/2004/XX/" + 
+          "http://docs.oasis-open.org/wss/2004/XX/" +
           "oasis-2004XX-wss-swa-profile-1.0#Attachment-Content-Only-Transform";
 
    protected String engineGetURI() {
        return implementedTransformURI;
    }
 
-   protected XMLSignatureInput enginePerformTransform(
-             XMLSignatureInput input)
-             throws TransformationException {
+   @Override
+   protected XMLSignatureInput enginePerformTransform(XMLSignatureInput input, OutputStream os,
+                                                      Element transformElement, String baseURI,
+                                                      boolean secureValidation)
+           throws IOException, CanonicalizationException, InvalidCanonicalizerException,
+           TransformationException, ParserConfigurationException, SAXException {
        try {
             return new XMLSignatureInput(_canonicalize(input));
        } catch (Exception e) {
             // log
             throw new TransformationException(e.getMessage(), e);
-       }  
+       }
    }
 
    private byte[] _canonicalize(XMLSignatureInput input) throws Exception {
        byte[] inputContentBytes = input.getBytes();
-       //ContentType contentType = new ContentType(((AttachmentSignatureInput)input).getContentType()); 
+       //ContentType contentType = new ContentType(((AttachmentSignatureInput)input).getContentType());
 
-       Canonicalizer canonicalizer = 
+       Canonicalizer canonicalizer =
                              CanonicalizerFactory.
                                    getCanonicalizer(((AttachmentSignatureInput)input).getContentType());
 
-       return canonicalizer.canonicalize(inputContentBytes);    
+       return canonicalizer.canonicalize(inputContentBytes);
    }
 
    public boolean wantsOctetStream ()   { return true; }
