7da76a591944d373f6c113ce4da0acff0323847e
Lukas Jungmann <lukas.jungmann@oracle.com>
2022-03-15 18:09:47 +0100

    Integrate Santuario 2.3.0
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/wss/impl/transform/AttachmentCompleteTransform.java
+++ wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/wss/impl/transform/AttachmentCompleteTransform.java
@@ -17,12 +17,18 @@ package com.sun.xml.wss.impl.transform;
 import com.sun.xml.wss.impl.c14n.Canonicalizer;
 import com.sun.xml.wss.impl.c14n.CanonicalizerFactory;
 import com.sun.xml.wss.impl.c14n.MimeHeaderCanonicalizer;
-
 import com.sun.xml.wss.impl.resolver.AttachmentSignatureInput;
-
-import org.apache.xml.security.transforms.TransformSpi;
+import org.apache.xml.security.c14n.CanonicalizationException;
+import org.apache.xml.security.c14n.InvalidCanonicalizerException;
 import org.apache.xml.security.signature.XMLSignatureInput;
+import org.apache.xml.security.transforms.TransformSpi;
 import org.apache.xml.security.transforms.TransformationException;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class AttachmentCompleteTransform extends TransformSpi {
 
@@ -36,9 +42,11 @@ public class AttachmentCompleteTransform extends TransformSpi {
    }
 
    @Override
-   protected XMLSignatureInput enginePerformTransform(
-             XMLSignatureInput input)
-             throws TransformationException {
+   protected XMLSignatureInput enginePerformTransform(XMLSignatureInput input, OutputStream os,
+                                                      Element transformElement, String baseURI,
+                                                      boolean secureValidation)
+           throws IOException, CanonicalizationException, InvalidCanonicalizerException,
+                  TransformationException, ParserConfigurationException, SAXException {
        try {
             return new XMLSignatureInput(_canonicalize(input));
        } catch (Exception e) {
