ad321ca837d69984764c01bf7555beee4cf8d24f
David Matějček <david.matejcek@omnifish.ee>
2023-01-05 23:40:05 +0100

    Lastest XmlSec updates - XMLSignatureInput was split
    - The class and it's children are now more simple and provide better control
      over the stream if the stream is used.
    - Formatting changes of ugly code
    
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/ws/security/opt/impl/incoming/URIResolver.java
+++ wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/ws/security/opt/impl/incoming/URIResolver.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
  * Copyright (c) 1997, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -18,8 +19,10 @@ import com.sun.xml.ws.security.opt.api.NamespaceContextInfo;
 import com.sun.xml.ws.security.opt.api.SecurityHeaderElement;
 import com.sun.xml.ws.security.opt.impl.JAXBFilterProcessingContext;
 import com.sun.xml.ws.security.opt.impl.crypto.AttachmentData;
+
 import java.util.ArrayList;
 import java.util.Iterator;
+
 import javax.xml.crypto.Data;
 import javax.xml.crypto.URIDereferencer;
 import javax.xml.crypto.URIReference;
@@ -31,8 +34,8 @@ import javax.xml.crypto.XMLCryptoContext;
  * @author K.Venugopal@sun.com
  */
 public class URIResolver implements URIDereferencer{
-    private SecurityContext securityContext;
-    private JAXBFilterProcessingContext pc = null;
+    private final SecurityContext securityContext;
+    private final JAXBFilterProcessingContext pc;
     /** Creates a new instance of resolver */
     public URIResolver(JAXBFilterProcessingContext pc) {
         this.pc = pc;
@@ -46,75 +49,76 @@ public class URIResolver implements URIDereferencer{
         HeaderList headers = (HeaderList) securityContext.getNonSecurityHeaders();
         String tmpId = uRIReference.getURI();
 
-        if(tmpId.startsWith("cid:")){
+        if (tmpId.startsWith("cid:")) {
             return dereferenceAttachments(tmpId.substring(4));
         }
 
         String id = "";
         int index = tmpId.indexOf("#");
-        if( index >=0){
-            id = tmpId.substring(index+1);
-        }else{
+        if (index >= 0) {
+            id = tmpId.substring(index + 1);
+        } else {
             id = tmpId;
         }
-        if(headers != null && headers.size() >0){
+        if (headers != null && !headers.isEmpty()) {
             Iterator<Header> listItr = headers.listIterator();
-            boolean found = false;
-            while(listItr.hasNext()){
-                GenericSecuredHeader header = (GenericSecuredHeader)listItr.next();
-                if(header.hasID(id) && !header.hasEncData()){
-                    return new StreamWriterData(header,((NamespaceContextInfo)header).getInscopeNSContext());
+            while (listItr.hasNext()) {
+                GenericSecuredHeader header = (GenericSecuredHeader) listItr.next();
+                if (header.hasID(id) && !header.hasEncData()) {
+                    return new StreamWriterData(header, ((NamespaceContextInfo) header).getInscopeNSContext());
                 }
             }
         }
 
         ArrayList pshList =  securityContext.getProcessedSecurityHeaders();
-        for(int j=0; j< pshList.size() ; j++){
-            SecurityHeaderElement  header = (SecurityHeaderElement) pshList.get(j);
-            if(id.equals(header.getId())){
-                if(header instanceof NamespaceContextInfo){
-                    return new StreamWriterData(header,((NamespaceContextInfo)header).getInscopeNSContext());
-                }else{
-                    throw new URIReferenceException("Cannot derefernce this MessagePart and use if for any crypto operation " +
-                              "as the message part is not cached");
+        for (Object element : pshList) {
+            SecurityHeaderElement header = (SecurityHeaderElement) element;
+            if (id.equals(header.getId())) {
+                if (header instanceof NamespaceContextInfo) {
+                    return new StreamWriterData(header, ((NamespaceContextInfo) header).getInscopeNSContext());
+                } else {
+                    throw new URIReferenceException(
+                        "Cannot derefernce this MessagePart and use if for any crypto operation "
+                            + "as the message part is not cached");
                 }
             }
         }
 
         // looking into buffered headers for - (Should be used only for getting the key)
         // What will happen when encrypting the content but signing the entire element? Can go wrong
-        ArrayList bufList =  securityContext.getBufferedSecurityHeaders();
-        for(int j=0; j< bufList.size() ; j++){
-            SecurityHeaderElement  header = (SecurityHeaderElement) bufList.get(j);
-            if(id.equals(header.getId())){
-                if(header instanceof NamespaceContextInfo){
-                    return new StreamWriterData(header,((NamespaceContextInfo)header).getInscopeNSContext());
-                }else{
-                    throw new URIReferenceException("Cannot derefernce this MessagePart and use if for any crypto operation " +
-                              "as the message part is not cached");
+        ArrayList bufList = securityContext.getBufferedSecurityHeaders();
+        for (Object element : bufList) {
+            SecurityHeaderElement header = (SecurityHeaderElement) element;
+            if (id.equals(header.getId())) {
+                if (header instanceof NamespaceContextInfo) {
+                    return new StreamWriterData(header, ((NamespaceContextInfo) header).getInscopeNSContext());
+                } else {
+                    throw new URIReferenceException(
+                        "Cannot derefernce this MessagePart and use if for any crypto operation "
+                            + "as the message part is not cached");
                 }
             }
         }
 
-        Data data = null;
-        data = (Data)pc.getSTRTransformCache().get(id);
-        if(data != null)
+        Data data = (Data) pc.getSTRTransformCache().get(id);
+        if (data != null) {
             return data;
-        data = (Data)pc.getElementCache().get(id);
-        return data;
+        }
+        return (Data) pc.getElementCache().get(id);
     }
 
-    private Data dereferenceAttachments(String cidRef)  throws URIReferenceException{
+
+    private Data dereferenceAttachments(String cidRef) throws URIReferenceException {
         AttachmentSet attachments = securityContext.getDecryptedAttachmentSet();
-        if(attachments == null || attachments.isEmpty()){
+        if (attachments == null || attachments.isEmpty()) {
             attachments = securityContext.getAttachmentSet();
         }
-        if(attachments == null || attachments.isEmpty()){
-            throw new URIReferenceException ("Attachment Resource with Identifier  "+cidRef+" was not found");
+        if (attachments == null || attachments.isEmpty()) {
+            throw new URIReferenceException("Attachment Resource with Identifier  " + cidRef + " was not found");
         }
         Attachment attachment = attachments.get(cidRef);
-        if(attachment == null){
-            throw new URIReferenceException ("Attachment Resource with Identifier  "+cidRef+" was not found");
+        if (attachment == null) {
+            throw new URIReferenceException("Attachment Resource with Identifier  " + cidRef + " was not found");
         }
         return new AttachmentData(attachment);
     }
