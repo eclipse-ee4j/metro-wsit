4ee6aa3acf10904b164d716ffb58c317d8cfe9ed
Lukas Jungmann <lukas.jungmann@oracle.com>
2023-10-11 16:47:24 +0200

    fix cp right
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/wss/jaxws/impl/SecurityServerTube.java
+++ wsit/ws-sx/wssx-impl/src/main/java/com/sun/xml/wss/jaxws/impl/SecurityServerTube.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2022 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2023 Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2022 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
