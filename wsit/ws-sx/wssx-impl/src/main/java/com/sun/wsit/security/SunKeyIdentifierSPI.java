f2c1d1e04dd923a1b14f967fe41cfd67c7804c82
Lukas Jungmann <lukas.jungmann@oracle.com>
2023-10-20 15:40:47 +0200

    fix docs build
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- wsit/ws-sx/wssx-impl/src/main/java/com/sun/wsit/security/SunKeyIdentifierSPI.java
+++ wsit/ws-sx/wssx-impl/src/main/java/com/sun/wsit/security/SunKeyIdentifierSPI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -10,46 +10,61 @@
 
 package com.sun.wsit.security;
 
-import java.io.IOException;
+import java.math.BigInteger;
 import java.security.cert.X509Certificate;
+import java.util.Arrays;
+
 /**
  *
  * @author sk112103
  */
 public class SunKeyIdentifierSPI extends com.sun.xml.wss.core.reference.KeyIdentifierSPI {
-    
+
+    /**
+     * total header length
+     */
+    private static final byte HEADER_LENGTH = 4;
+
+    /**
+     * length of a header value
+     */
+    private static final byte HEADER_FIELD_LENGTH = 1;
+
+    /**
+     * position (0-based) of the header field that contains the expected length of the actual value in byte
+     */
+    private static final byte HEADER_FIELD_VALUE_LENGTH = 3;
+
     /** Creates a new instance of SunKeyIdentifierSPI */
     public SunKeyIdentifierSPI() {
     }
 
     public byte[] getSubjectKeyIdentifier(X509Certificate cert) throws KeyIdentifierSPIException {
-        byte[] subjectKeyIdentifier =
-                cert.getExtensionValue(SUBJECT_KEY_IDENTIFIER_OID);
-        if (subjectKeyIdentifier == null)
+        byte[] subjectKeyIdentifier = cert.getExtensionValue(SUBJECT_KEY_IDENTIFIER_OID);
+        if (subjectKeyIdentifier == null) {
             return null;
-        
+        }
+
         try {
-            sun.security.x509.KeyIdentifier keyId = null;
-            
-            sun.security.util.DerValue derVal = new sun.security.util.DerValue(
-                    new sun.security.util.DerInputStream(subjectKeyIdentifier).getOctetString());
-            
-            keyId = new sun.security.x509.KeyIdentifier(derVal.getOctetString());
-            return keyId.getIdentifier();
-        } catch (NoClassDefFoundError ncde) {
-            // TODO X509 Token profile states that only the contents of the
-            // OCTET STRING should be returned, excluding the "prefix"
-            byte[] dest = new byte[subjectKeyIdentifier.length-4];
-            System.arraycopy(
-                    subjectKeyIdentifier, 4, dest, 0, subjectKeyIdentifier.length-4);
-            return dest;
-            
-        } catch (IOException e) {
+            if (subjectKeyIdentifier.length < HEADER_LENGTH) {
+                throw new IllegalArgumentException("subjectKeyIdentifier too short, header is missing");
+            }
+
+            final int valueLength = (new BigInteger(
+                    Arrays.copyOfRange(subjectKeyIdentifier, HEADER_FIELD_VALUE_LENGTH, HEADER_FIELD_VALUE_LENGTH + HEADER_FIELD_LENGTH)))
+                    .intValue();
+            final int expectedTotalLength = HEADER_LENGTH + valueLength;
+            if (subjectKeyIdentifier.length < expectedTotalLength) {
+                throw new IllegalArgumentException(String.format("subjectKeyIdentifier too short (expected=%d, actual=%d)", expectedTotalLength, subjectKeyIdentifier.length));
+            } else if (subjectKeyIdentifier.length > expectedTotalLength) {
+                throw new IllegalArgumentException(String.format("subjectKeyIdentifier too big (expected=%d, actual=%d)", expectedTotalLength, subjectKeyIdentifier.length));
+            }
+            return Arrays.copyOfRange(subjectKeyIdentifier, HEADER_LENGTH, HEADER_LENGTH + valueLength);
+
+        } catch (Exception e) {
             //log exception
             throw new KeyIdentifierSPIException(e);
         }
-
     }
-    
-    
+
 }
