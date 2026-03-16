547e96ce1b729bd4bfd12302cfb4ab29b9df0ccd
David Matějček <david.matejcek@omnifish.ee>
2026-03-16 15:11:49 +0100

    Updated gmbal - fixes issues with its JPMS
    - We need the new JAX-WS-RI here, with fixes
    - Fixed module-info of metro-cm-impl
    - Updated Tomcat used on CI
    - Updated spotbugs
    
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- wsit/metro-cm/metro-cm-impl/src/main/java/module-info.java
+++ wsit/metro-cm/metro-cm-impl/src/main/java/module-info.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
  * Copyright (c) 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -9,6 +10,9 @@
  */
 
 module org.glassfish.metro.cm.impl {
+
+    requires com.sun.istack.runtime;
+    requires com.sun.xml.ws.rt;
     requires org.glassfish.metro.cm.api;
 
     provides com.sun.xml.ws.api.config.management.ManagedEndpointFactory with
